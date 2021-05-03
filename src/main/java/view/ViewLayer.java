9d922e1009335dacfa39abb26047f5736b5a90a0
Jrock30 <jrg8989@knou.ac.kr>
2021-05-03 21:54:19 +0900

    feat: View Layer 구현

---
+++ src/main/java/view/ViewLayer.java
@@ -0,0 +1,11 @@
+package view;
+
+public interface ViewLayer {
+
+    String inputValue();
+
+    void outValue(String value);
+
+    void outValueLine(String value);
+
+}
---
+++ src/main/java/view/ViewLayerImpl.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.util.Scanner;
+
+public class ViewLayerImpl implements ViewLayer {
+
+
+    @Override
+    public String inputValue() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    @Override
+    public void outValue(String value) { System.out.print(value); }
+
+    @Override
+    public void outValueLine(String value) { System.out.println(value); }
+}
