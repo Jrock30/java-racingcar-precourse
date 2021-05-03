c402d4f9af971c8faaa3c2a4f075746f571766a4
Jrock30 <jrg8989@knou.ac.kr>
2021-05-03 21:54:50 +0900

    feat: 상수 클래스 추가

---
+++ src/main/java/util/Const.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Const {
+
+    public static final int CAR_NAME_SIZE = 5;
+    public static final int RANDOM_MAX_NUMBER = 8;
+    public static final int ADVANCE_NUMBER = 4;
+    public static final int ROUND_ADD_NUMBER = 1;
+    public static final String ROUND_DISPLAY = "-";
+}
